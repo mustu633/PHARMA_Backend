5a6f7852e16068bf99891c1beab187888b4506ec
mustu633 <mustafakamal63633@gmail.com>
2025-07-28 16:11:26 -0700

    Initial backend commit

---
+++ models/productSchema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  category: { type: String, required: true },
+  supplier: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Supplier",
+    required: true,
+  },
+  desc: { type: String },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  reorderLevel: { type: Number, default: 10 },
+  barcode: { type: String, unique: true },
+  C: {type: String},
+  R: {type: String},
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
